cdaf24a4b9c26ed9150a8792719b623489bcdbfd
egualdo <colmenares203@gmail.com>
2026-01-20 20:21:30 -0400

    console routes to cron job

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command('rates:update')->everyFiveMinutes()->runInBackground()->withoutOverlapping(5);
+
+// Schedule::command('get:data-competitors')->monthlyOn(5, '12:10')->runInBackground()->withoutOverlapping(5);
+// Schedule::command('get:make-competitor-conclusions')->monthlyOn(5, '12:15')->runInBackground()->withoutOverlapping(5);
+// Schedule::command('get:make-client-conclusions')->monthlyOn(5, '12:20')->runInBackground()->withoutOverlapping(5);
+// Schedule::command('get:make-versus-conclusions')->monthlyOn(5, '12:25')->runInBackground()->withoutOverlapping(5);
+// Schedule::command('get:googledata')->monthlyOn(5, '12:30')->runInBackground()->withoutOverlapping(5);
+// Schedule::command('make:process-pending-logs')->cron('0,30 * 4-6 * *')->withoutOverlapping(10);
+// Schedule::command('test:daily-log')->daily();
